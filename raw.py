a4f74e8d776c04ff13f50e4512f50acdd155d55b
arkit3kt <exodust@protonmail.com>
2020-08-31 22:20:03 -0400

    refac

--- raw.py
+++ raw.py
@@ -1,68 +1,79 @@
 #!/usr/bin/env python3
 import requests
-
+from time import time
 class raw:
 
     def __init__(self, headers, client_id, secret):
-        self.url = "https://ruqqus.com/"
-        self.headers = headers
+        self.url = "https://ruqqus.com"
         self.client_id = client_id
         self.client_secret = secret
+        self.token_expire_utc = 0
+        self.refresh_token = None
+        #self.getAccessToken(code=ABCDEFG)
 
     def get(self, *args, **kwargs):
+
         type = kwargs['type'].lower()
+        self.getHeaders(user_agent="tester by @arkitekt")
+
 
         if type == "front":
-            return requests.get(url=f"{self.url}api/v1/front/listing",
+            return requests.get(url=f"{self.url}/api/v1/front/listing",
                                 headers=self.headers).json()
 
         elif type == "guild":
-            return requests.get(url=f"{self.url}api/v1/guild/{kwargs['guildName']}/listing",
+            return requests.get(url=f"{self.url}/api/v1/guild/{kwargs['guildName']}/listing",
                                 headers=self.headers).json()
 
         elif type == "user":
-            return requests.get(url=f"{self.url}api/v1/user/{kwargs['username']}",
+            return requests.get(url=f"{self.url}/api/v1/user/{kwargs['username']}",
                                 headers=self.headers).json()
 
         elif type == "post":
-            return requests.get(url=f"{self.url}api/v1/post/{kwargs['postId']}",
+            return requests.get(url=f"{self.url}/api/v1/post/{kwargs['postId']}",
                                 headers=self.headers).json()
 
         elif type == "comment":
-            return requests.get(url = f"{self.url}api/v1/comment/{kwargs['commentId']}",
+            return requests.get(url = f"{self.url}/api/v1/comment/{kwargs['commentId']}",
                                headers=self.headers).json()
 
-    def refreshToken(self):
+    def getHeaders(self, *args, **kwargs):
+
+        # refresh token 30 seconds before expiration
+        if self.token_expire_utc >= int(time() - 30):
+            self.refreshToken()
+
+        self.headers = {"Authorization": "Bearer " + self.access_token,
+                        "user-agent": kwargs['user_agent']
+                        }
+
+    def refreshToken(self, *args, **kwargs):
+
         data = {"client_id": self.client_id,
                 "client_secret": self.client_secret,
                 "grant_type": "refresh",
                 "refresh_token": self.refresh_token
                 }
 
-        r = requests.post(url=f"{self.url}oauth/grant",
+        r = requests.post(url=f"{self.url}/oauth/grant",
                              headers=self.headers,
                              data=data).json()
 
         self.access_token = r['access_token']
-
-        self.headers = {"Authorization": "Bearer " + self.access_token,
-                        "user-agent": self.headers['user_agent']
-                        }
         return r
-    def getAccessToken(self, code):
+
+    def getAccessToken(self, *args, **kwargs):
+        self.getHeaders()
+
         data = {"client_id": self.client_id,
                 "client_secret": self.client_secret,
                 "grant_type": "code",
                 "code": code
                 }
 
-        r = requests.post(url=f"{self.url}oauth/grant",
-                          headers=headers,
+        r = requests.post(url=f"{self.url}/oauth/grant",
+                          headers=self.headers,
                           data=data).json()
 
         self.access_token = r['access_token']
-
-        self.headers = {"Authorization": "Bearer " + self.access_token,
-                        "user-agent": self.headers['user_agent']
-                        }
         return r
